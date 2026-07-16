633c25a2657c3eabb2feab0e33886ffff46be74b
luffy4589 <166809481+luffy4589@users.noreply.github.com>
2026-07-16 23:50:58 +0530

    Update 268(Missing Number).java

--- 268(Missing Number).java
+++ 268(Missing Number).java
@@ -1,3 +1,4 @@
+//Using Natural numbers sum formula
 class Solution {
     public int missingNumber(int[] nums) {
       int n=nums.length,s=0;
@@ -6,5 +7,21 @@ class Solution {
       }
       int t=(n*(n+1))/2;
       return t-s;
+    }
+}
+
+//Using additional array
+class Solution {
+    public int missingNumber(int[] nums) {
+      int n=nums.length,s=0;
+      int a[]=new int[n+1];
+      Arrays.fill(a,-1);
+      for(int i=0;i<n;i++){
+        a[nums[i]]=nums[i];
+      }
+      for(int i=0;i<a.length;i++){
+        if(a[i]==-1)  return i;
+      }
+      return 0;
     }
 }
