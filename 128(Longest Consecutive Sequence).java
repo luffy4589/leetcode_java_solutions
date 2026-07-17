cd34f300cf443d8b396b97915864631f0f5dca9b
luffy4589 <166809481+luffy4589@users.noreply.github.com>
2026-07-17 20:44:07 +0530

    Update 128(Longest Consecutive Sequence).java

--- 128(Longest Consecutive Sequence).java
+++ 128(Longest Consecutive Sequence).java
@@ -23,3 +23,26 @@ class Solution {
       return false;
     }
 }
+
+//O(nlogn)
+class Solution {
+    public int longestConsecutive(int[] nums) {
+      int n=nums.length;
+      if(n==0)  return 0;
+      Arrays.sort(nums);
+      int lcs=0,currlcs=1;
+      for(int i=1;i<n;i++){
+        if(nums[i]==nums[i-1]){
+          continue;
+        }
+        if(nums[i]==nums[i-1]+1){
+          currlcs++;
+        }
+        else{
+          lcs=Math.max(lcs,currlcs);
+          currlcs=1;
+        }
+      }
+      return Math.max(lcs,currlcs); 
+    }
+}
