841640b05b75bae46b9254e2206e4a9bb7a44e9e
luffy4589 <166809481+luffy4589@users.noreply.github.com>
2026-07-27 12:03:53 +0530

    Update 1464(Maximum Product of Two Elements in an Array).java

--- 1464(Maximum Product of Two Elements in an Array).java
+++ 1464(Maximum Product of Two Elements in an Array).java
@@ -22,3 +22,21 @@ class Solution {
       return ((nums[nums.length-1]-1)*(nums[nums.length-2]-1));
     }
 }
+
+//optimal
+class Solution {
+    public int maxProduct(int[] nums) {
+      //int n=nums.length;
+      int m1=0,m2=0;
+      for(int n:nums){
+        if(n>m1){
+          m2=m1;
+          m1=n;
+        }
+        else if(n>m2){
+          m2=n;
+        }
+      }
+      return (m1-1)*(m2-1);
+    }
+}
